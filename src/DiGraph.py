a340aec63653023bdbcf04310598989cd28b9578
porat-ah <porat2001@gmail.com>
2021-01-10 21:54:41 +0200

    addind doc

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -11,57 +11,67 @@ class DiGraph(GraphInterface):
     def getTranspose(self):
         return self.transpose
         """
-        *Get transposition status .
+        @return transposition status .
         """
 
     def setTranspose(self):
         self.transpose = not self.transpose
         """
-        *Set transposition status.
+        Set transposition status.
         """
 
     def v_size(self) -> int:
         return len(self.nodes)
         """
-        Returns the number of vertices in this graph
         @return: The number of vertices in this graph
         """
 
     def e_size(self) -> int:
         return self.edgeSize
         """
-        Returns the number of edges in this graph
         @return: The number of edges in this graph
         """
 
     def get_all_v(self) -> dict:
         return self.nodes
-        """return a dictionary of all the nodes in the Graph, each node is represented using a pair
-         (node_id, node_data)
+        """
+        @return a dictionary of all the nodes in the Graph
         """
 
     def all_in_edges_of_node(self, id1: int) -> dict:
-        try:
-            return self.nodes[id1].getSrc()
-        except:
-            raise ValueError
-        """return a dictionary of all the nodes connected to (into) node_id ,
-        each node is represented using a pair (other_node_id, weight)
+        if(self.transpose):
+             try:
+                 return self.nodes[id1].getSrc()
+             except:
+                 raise ValueError
+        else: 
+            try:
+                return self.nodes[id1].getDest()
+            except:
+                raise ValueError
+        """
+        @return a dictionary of all the nodes connected to (into) node_id
          """
 
     def all_out_edges_of_node(self, id1: int) -> dict:
-        try:
-            return self.nodes[id1].getDest()
-        except:
-            raise ValueError
-        """return a dictionary of all the nodes connected from node_id , each node is represented using a pair
-        (other_node_id, weight)
+        if(self.transpose):
+             try:
+                 return self.nodes[id1].getDest()
+             except:
+                 raise ValueError
+        else: 
+            try:
+                return self.nodes[id1].getSrc()
+            except:
+                raise ValueError
+        """
+        @return a dictionary of all the nodes connected from node_id
         """
 
     def get_mc(self) -> int:
+        return self.mc
         """
         Returns the current version of this graph,
-        on every change in the graph state - the MC should be increased
         @return: The current version of this graph.
         """
         raise NotImplementedError
@@ -73,8 +83,6 @@ class DiGraph(GraphInterface):
         @param id2: The end node of the edge
         @param weight: The weight of the edge
         @return: True if the edge was added successfully, False o.w.
-
-        Note: If the edge already exists or one of the nodes dose not exists the functions will do nothing
         """
         raise NotImplementedError
 
@@ -84,8 +92,6 @@ class DiGraph(GraphInterface):
         @param node_id: The node ID
         @param pos: The position of the node
         @return: True if the node was added successfully, False o.w.
-
-        Note: if the node id already exists the node will not be added
         """
         raise NotImplementedError
 
@@ -94,8 +100,6 @@ class DiGraph(GraphInterface):
         Removes a node from the graph.
         @param node_id: The node ID
         @return: True if the node was removed successfully, False o.w.
-
-        Note: if the node id does not exists the function will do nothing
         """
         raise NotImplementedError
 
@@ -105,7 +109,5 @@ class DiGraph(GraphInterface):
         @param node_id1: The start node of the edge
         @param node_id2: The end node of the edge
         @return: True if the edge was removed successfully, False o.w.
-
-        Note: If such an edge does not exists the function will do nothing
         """
         raise NotImplementedError
--- src/Node.py
+++ src/Node.py
@@ -6,22 +6,33 @@ class Node:
         self.src = {}  # a dict of all edges where this node is in the start of the edge
 
     def setPos(self, x):
-        if (type(x) == tuple and len(x) == 3):
-            self.pos = x
-        else:
-            raise TypeError()
-
+        self.pos = x
+    """
+    set the position of the node
+    """
     def getId(self):
         return self.id
+    """
+    @return the id of the node
+    """
 
     def getPos(self):
         return self.pos
+    """
+    @return the position of the node
+    """
 
-    def setCrc(self, id, w):
+    def setSrc(self, id, w):
         self.src.update({id: w})
+        """
+        add an edge to the list of all edges where this node is their src
+        """
 
     def setDest(self, id, w):
         self.dest.update({id: w})
+        """
+        add an edge to the list of all edges where this node is their dest
+        """
 
     def getSrc(self):
         return self.src
